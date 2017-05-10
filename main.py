db68e6a7148f206be8dc325c1380d4d8784eb2cb
Tim Mironov <timirlan666@gmail.com>
2017-05-10 09:54:47 +0500

    + main.py and .gitignore

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+Решение тестового задания.
+"""
+
+__author__ = "Tim Mironov"
+__email__ = "timirlan666@gmail.com"
+
+def collect_rpi_into_csv() -> None:
+    """
+    Основная фунция. Собирает rpi по дням и создаёт файл с результатом.
+    """
+    pass
+
+if __name__ == "__main__":
+    collect_rpi_into_csv()
